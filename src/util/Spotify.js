198bda0eec4c7d3d179ef65ee65cbdfb428d399a
gtuckerBEE <46637036+gtuckerBEE@users.noreply.github.com>
2019-03-05 16:12:47 -0700

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,73 @@
-let userAccessToken= '';
+const accessToken = '';
+const clientID = '36b7ea3dbddc4799a1aee3995f9e6f1a';
+const redirectURI = "http://localhost:3000/";
 
 const Spotify = {
+  search(term) {
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+  headers: {Authorization: `Bearer ${accessToken}`}
+})
+},
+
   getAccessToken() {
-    if(userAccessToken) {
-      return;
-    }
+  if(accessToken)
+    return accessToken;
+  else if(window.location.href.match(/access_token=([^&]*)/) && window.location.href.match(/expires_in=([^&]*)/))
+  {
+    let accessToken = window.location.href.match(/access_token=([^&]*)/)[1];
+    let expiresIn = window.location.href.match(/expires_in=([^&]*)/)[1];
+
+    window.setTimeout(() => accessToken = '', expiresIn*1000);
+    window.history.pushState('Access Token', null, '/');
+
+    return accessToken;
   }
+  else
+  {
+    let url = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+    window.location = url;
+  }
+ },
+
+ savePlaylist(name, trackUris) {
+
+   if (!name || !trackUris.length) {
+     return;
+   }
+   const accessToken = Spotify.getAccessToken();
+   const headers = { Authorization: `Bearer ${accessToken}` };
+   let userId;
+
+   return fetch('https://api.spotify.com/v1/me', {
+     headers: headers}
+   ).then(response => {
+     if (response.ok) {
+       return response.json()
+     }
+     throw new Error('Request Failed!');
+   }, networkError => console.log(networkError.message)
+   ).then(jsonResponse => {
+     userId = jsonResponse.id;
+     return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+       headers: headers,
+       method: 'POST',
+       body: JSON.stringify({name: name})
+     }).then(response => {
+       if (response.ok) {
+         return response.json()
+       }
+       throw new Error('Request Failed!');
+     }, networkError => console.log(networkError.message)
+     ).then(jsonResponse => {
+       const playlistId = jsonResponse.id;
+       return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+         headers: headers,
+         method: 'POST',
+         body: JSON.stringify({uris: trackUris})
+       });
+     })
+   })
+ }
 }
 
 export default Spotify;
