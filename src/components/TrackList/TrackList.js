27d65bd1219e7e210d2d9b70cf94c5ea10990d74
gtuckerBEE <46637036+gtuckerBEE@users.noreply.github.com>
2019-03-05 16:12:03 -0700

    Update TrackList.js

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -9,8 +9,8 @@ class TrackList extends React.Component {
   render() {
     return (
     <div className="TrackList">
-    {this.props.playlistTracks.map(track => {
-      return <Track playlistTracks={this.props.playlistTracks} isRemoval={this.props.isRemoval} onRemove={this.props.onRemove} onAdd={this.props.onAdd} track={track} key={track.id} />
+    {this.props.tracks.map(track => {
+      return <Track isRemoval={this.props.isRemoval} onRemove={this.props.onRemove} onAdd={this.props.onAdd} track={track} key={track.id} />
     })}
 </div>
   )}
