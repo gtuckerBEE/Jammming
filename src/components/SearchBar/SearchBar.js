d59dc363b8360ec86908802d35f441fdc6aedac7
gtuckerBEE <46637036+gtuckerBEE@users.noreply.github.com>
2019-03-07 14:12:18 -0700

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {term: ''}
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
@@ -13,15 +14,14 @@ class SearchBar extends React.Component {
   }
 
   handleTermChange(e) {
-    let term = e.target.value;
-    this.props.onChange(term);
+    this.setState = { term: e.target.value}
   }
 
   render() {
     return (
     <div className="SearchBar">
   <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
-  <a>SEARCH</a>
+  <a onClick={this.search}>SEARCH</a>
 </div>
   )}
 };
